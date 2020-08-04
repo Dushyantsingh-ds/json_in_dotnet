b4f791a89b0aac0e6984ce43189b4c7329976373
Dushyant Singh <55306442+Dushyantsingh-ds@users.noreply.github.com>
2020-08-05 01:17:57 +0530

    Add files via upload

---
+++ Web form/CS.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Net;
+using System.Data;
+using Newtonsoft.Json;
+
+public partial class CS : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            string json = (new WebClient()).DownloadString("https://raw.githubusercontent.com/aspsnippets/test/master/Customers.json");
+            GridView1.DataSource = JsonConvert.DeserializeObject<DataTable>(json);
+            GridView1.DataBind();
+        }
+    }
+}
---
+++ Web form/VB.aspx.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Net
+Imports System.Data
+Imports Newtonsoft.Json
+Partial Class VB
+    Inherits System.Web.UI.Page
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        If Not Me.IsPostBack Then
+            Dim json As String = (New WebClient).DownloadString("https://raw.githubusercontent.com/aspsnippets/test/master/Customers.json")
+            GridView1.DataSource = JsonConvert.DeserializeObject(Of DataTable)(json)
+            GridView1.DataBind()
+        End If
+    End Sub
+End Class
